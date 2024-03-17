e84a6ee806820f503403bf73083f489056de739a
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2024-03-17 10:26:22 -0700

    20240317-Update

--- Lab10/book_order.py
+++ Lab10/book_order.py
@@ -5,21 +5,32 @@ from book_order_utils import *
 
 def main():
     order_num = input("Enter the order number: ")
+    sys.argv.append(order_num)
     title = input("Enter the book title: ")
+    sys.argv.append(title)
     author = input("Enter the book author: ")
+    sys.argv.append(author)
     isbn = input("Enter the book ISBN: ")
-    year_pub = input("Enter the year of publication: ")
+    sys.argv.append(isbn)
+    year_pub = input("Enter the year the publication: ")
+    sys.argv.append(year_pub)
     quantity = input("Enter the order quantity: ")
-    cost_cad = input("Enter the order cost: ")
+    sys.argv.append(quantity)
+    cost_cad = input("Enter the order_cost: ")
+    sys.argv.append(cost_cad)
+    print(len(sys.argv))
 
     if len(sys.argv) != 8:
-        raise ValueError("Command line arguments do not match requirement")
+        raise ValueError("Number of command line arguments does not match requirement")
 
     try:
-        validate_book_order_details(order_num, title, author, isbn, year_pub, quantity, cost_cad)
-        unit_cost_cad = calculate_per_book_cost_cad(cost_cad, quantity)
+        validate_book_order_details(int(order_num), title, author, int(isbn),
+                                    int(year_pub), int(quantity), float(cost_cad))
+
+        unit_cost_cad = calculate_per_book_cost_cad(float(cost_cad), int(quantity))
         write_book_order_details("book_details.txt", title, author, isbn,
                                  year_pub, quantity, cost_cad, unit_cost_cad)
+
     except ValueError:
         ValueError("Value Error: ")
     except TypeError:
---
+++ Lab10/test.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    print("main")
+    print (f"your age is {1890}")
+    input("Place: ")
+    print(sys.argv)
+
+
+main()
